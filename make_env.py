419088ef72014a0cda270d5f16702f179739c281
Paul Carpenter <paul.carpenter@bsc.es>
2020-10-26 12:04:01 +0100

    Removed option to time it

--- make_env.py
+++ make_env.py
@@ -57,10 +57,9 @@ def main(argv):
     dotfile = None
     seed = 1
     doall = False
-    timeit = False
     method = 'matching'
     try:
-        opts, args = getopt.getopt( argv[1:], 'h', ['help', 'dot=', 'seed=', 'all', 'time', 'method='])
+        opts, args = getopt.getopt( argv[1:], 'h', ['help', 'dot=', 'seed=', 'all', 'method='])
     except getopt.error, msg:
         print msg
         print "for help use --help"
@@ -74,76 +73,13 @@ def main(argv):
             seed = int(a)
         if o == '--all':
             doall = True
-        if o == '--time':
-            timeit = True
         if o == '--method':
             method = a
             if not method in ['config', 'greedy', 'matching']:
                 return Usage(argv)
     random.seed(seed)
 
-    if timeit:
-        min_nodes = 2
-        max_nodes = 3000
-        step = 1.8 # Multiply by this each time
-
-        colors = ['black', 'red', 'olivedrab', 'saddlebrown', 'blue']
-        linestyles = ['-', '--']
-
-        num_nodes = [ int(0.5+math.exp(e)) for e in numpy.arange( math.log(min_nodes), math.log(max_nodes), math.log(step) ) ]
-        if num_nodes[-1] != max_nodes:
-            num_nodes.append(max_nodes)
-
-        series = []
-
-        for idx2,method in enumerate(['greedy', 'matching']):
-            maxnodes = max(num_nodes)
-            for idx,deg in enumerate([2,3,4,6,8]):
-                nn = []
-                tt = []
-                for n in num_nodes:
-                    if n >= deg and n <= maxnodes:
-                        start_time =  time.time()
-                        numexp = 10
-                        ne = 0
-                        try:
-                            for exp in range(0, numexp):
-                                G = solve.generate_random_bipartite(n, deg, exp, method=method)
-                                ne += 1
-                                if time.time() > start_time + 60:
-                                    break
-                            timeeach = (time.time() - start_time) / ne
-                        except ValueError:
-                            pass
-                        print method, 'nodes', n, 'deg', deg, 
-                        if ne == 0:
-                            print 'gave up'
-                            maxnodes = n-1 # Don't try same or more nodes again
-                        else:
-                            print 'time to generate %5f' % timeeach, '(ne=', ne, ')'
-                            nn.append(n)
-                            tt.append(timeeach)
-                series.append( (nn, tt, '%s degree %d' % (method,deg), colors[idx], linestyles[idx2]) )
-
-            with PdfPages('time-gen-topology-log.pdf') as pdf:
-                for xx,yy,label, col,ls in series:
-                    plt.semilogy(xx, yy, label = label, color=col, linestyle=ls)
-                plt.legend()
-                plt.xlabel('Number of nodes')
-                plt.ylabel('Time to generate topology graph (seconds)')
-                pdf.savefig()
-                plt.close()
-
-            with PdfPages('time-gen-topology.pdf') as pdf:
-                for xx,yy,label, col,ls in series:
-                    plt.plot(xx, yy, label = label, color=col, linestyle=ls)
-                plt.legend()
-                plt.xlabel('Number of nodes')
-                plt.ylabel('Time to generate topology graph (seconds)')
-                pdf.savefig()
-                plt.close()
-
-    elif not doall:
+    if not doall:
         if len(args) != 2:
             return Usage(argv)
 
