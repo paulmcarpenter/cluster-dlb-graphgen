917575002462d7807d98c7e8151bcf938f4faaa8
Paul Carpenter <paul.carpenter@bsc.es>
2020-11-03 07:41:07 +0100

    Faster C calc_iso

--- calc_iso.c
+++ calc_iso.c
@@ -7,7 +7,6 @@
 typedef unsigned long long node_bitmask_t;
 
 float iso = 1.0;
-char buf[100] = {0,};
 
 void sub(node_bitmask_t *adj, node_bitmask_t nodes_used, int vranks_used, int total_vranks, int vrank_next, int num_all_nodes)
 {
@@ -16,24 +15,15 @@ void sub(node_bitmask_t *adj, node_bitmask_t nodes_used, int vranks_used, int to
         float val = 1.0 * num_n / vranks_used;
         if (val < iso) {
             iso = val;
-#if 0
-            printf("Used vranks: %d are ", vranks_used);
-            for(int i=0; i<vranks_used; i++) {
-                printf("%d ", buf[i]);
-            }
-            printf(";  nodes %llx num_nodes: %d val: %f\n", nodes_used, num_n, val);
-#endif
         }
     }
     if (num_n < num_all_nodes && vranks_used < num_all_nodes) {
         // Choose next item to add
         for (int vrank = vrank_next; vrank < total_vranks; vrank++) {
-            buf[vranks_used] = vrank;
             node_bitmask_t new_nodes_used = nodes_used | adj[vrank];
             sub(adj, new_nodes_used, vranks_used + 1, total_vranks, vrank+1, num_all_nodes);
         }
     }
-
 }
 
 int main(int argc, char **argv)
