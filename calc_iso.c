99f8ea866c09374267fb070d5c8f70169f63b868
Paul Carpenter <paul.carpenter@bsc.es>
2021-06-04 19:16:12 +0200

    Also counts the numbers of cycles

--- calc_iso.c
+++ calc_iso.c
@@ -26,6 +26,98 @@ void sub(node_bitmask_t *adj_vrank, node_bitmask_t nodes_used, int num_vranks_us
     }
 }
 
+#define MAX_CYCLE_LEN 16
+
+static int cycle_count[MAX_CYCLE_LEN+1] = {0,};
+
+void build_cycle(node_bitmask_t *adj_vrank,
+                 vrank_bitmask_t *adj_node,
+				 vrank_bitmask_t vranks_used,
+				 node_bitmask_t nodes_used,
+				 int total_vranks,
+				 int num_nodes,
+				 int first_vrank_bit,
+				 int cur_vrank,
+				 int cycle_len)
+{
+	// Choose next node
+	node_bitmask_t next_nodes = adj_vrank[cur_vrank] & ~nodes_used;
+	while (next_nodes) {
+		int next_node = __builtin_ctzll(next_nodes);
+		next_nodes &= ~ (1 << next_node);
+		// printf(" Next node = %d\n", next_node);
+
+		if (adj_node[next_node] & first_vrank_bit) {
+			// printf("Found cycle of len %d\n", cycle_len + 1);
+			cycle_count[cycle_len+1] ++;
+		}
+		if (cycle_len+1 < MAX_CYCLE_LEN) {
+			// Choose next vrank
+			vrank_bitmask_t next_vranks = adj_node[next_node] & ~vranks_used;
+			while (next_vranks) {
+				int next_vrank = __builtin_ctzll(next_vranks);
+				next_vranks &= ~ (1 << next_vrank);
+				// printf(" Next vrank = %d\n", next_vrank);
+				build_cycle(adj_vrank,
+							adj_node,
+							vranks_used | (1ULL << next_vrank),
+							nodes_used | (1ULL << next_node),
+							total_vranks,
+							num_nodes,
+							first_vrank_bit,
+							next_vrank,
+							cycle_len+2);
+			}
+		}
+	}
+}
+
+void count_cycles(node_bitmask_t *adj_vrank, vrank_bitmask_t *adj_node, int total_vranks, int num_nodes)
+{
+	int first_vrank, second_vrank;
+	// Choose first vrank
+	for(first_vrank=0; first_vrank < total_vranks-1; first_vrank++) {
+		node_bitmask_t first_nodes = adj_vrank[first_vrank];
+		// printf("first_vrank = %d, nodes = %llx\n", first_vrank, first_nodes);
+		while (first_nodes) {
+			int first_node = __builtin_ctzll(first_nodes);
+			// printf(" First node = %d\n", first_node);
+			assert(first_nodes & (1 << first_node));
+			first_nodes &= ~ (1 << first_node);
+
+			// Choose second vrank (strictly after first to avoid double-counting)
+			vrank_bitmask_t second_vranks = adj_node[first_node];
+			while (second_vranks) {
+				int second_vrank = __builtin_ctzll(second_vranks);
+				assert(second_vranks & (1 << second_vrank));
+				second_vranks &= ~ (1 << second_vrank);
+				if (second_vrank > first_vrank) {
+					// printf("%d -> [%d] -> %d\n", first_vrank, first_node, second_vrank);
+
+					vrank_bitmask_t vranks_used = ((1ULL << (first_vrank+1))-1ULL) // all vranks up to and including first_vrank
+												  | (1LL << second_vrank);  // and second vrank
+					node_bitmask_t nodes_used = (1ULL << first_node);
+					build_cycle(adj_vrank,
+								adj_node,
+								vranks_used,
+								nodes_used,
+								total_vranks,
+								num_nodes,
+								1ULL << first_vrank,
+								second_vrank,
+								3);
+				}
+			}
+		}
+	}
+	for (int len=0; len <= MAX_CYCLE_LEN; len+=2) {
+		assert(cycle_count[len-1] == 0);
+		assert((cycle_count[len] % 2) == 0);
+		cycle_count[len] /= 2;
+	}
+}
+
+
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -81,8 +173,13 @@ int main(int argc, char **argv)
     iso = 1.0;
     //  adj  nodes-used    #vranks   total-vranks   vrank_next  num_all_nodes
     sub(adj_vrank, nodes_used,   0,        num_vranks,    0,          num_nodes);
+    printf("Isoperimetric number: %f\n", iso);
 
-    printf("%f\n", iso);
+	count_cycles(adj_vrank, adj_node, num_vranks, num_nodes);
+
+	for(int len=2; len <= MAX_CYCLE_LEN; len+=2) {
+		printf("Number of cycles of length %d: %d\n", len, cycle_count[len]);
+	}
 
     return 0;
 }
